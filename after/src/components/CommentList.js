b0681713abc7e7b75947dc8ad302110033ce9d6a
Rakesh Kumar 10 <rakkumar0@WKMZTBDE5D14.global.publicisgroupe.net>
2024-05-29 20:59:10 +0530

    applyed isp

--- after/src/components/CommentList.js
+++ after/src/components/CommentList.js
@@ -1,32 +1,39 @@
-import React, {useState, useEffect} from 'react'
-import axios from 'axios';
-import Loading from './Loading';
-import CommentItems from './CommentItems';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Loading from "./Loading";
+import CommentItems from "./CommentItems";
 
-function CommentList({postId}) {
-    const [comments, setComments] = useState([]);
+function CommentList({ postId }) {
+  const [comments, setComments] = useState([]);
 
-    useEffect(() => {
-        async function fetchComments() {
-            const response = await axios.get(`https://jsonplaceholder.typicode.com/posts/${postId}/comments?_limit=2`);
-            setComments(response.data);
-        }
+  useEffect(() => {
+    async function fetchComments() {
+      const response = await axios.get(
+        `https://jsonplaceholder.typicode.com/posts/${postId}/comments?_limit=2`
+      );
+      setComments(response.data);
+    }
 
-        fetchComments();
-    }, [postId]);
-  
-    if (!comments?.length) return <Loading>Comments loading...</Loading>;
-  
+    fetchComments();
+  }, [postId]);
+
+  if (!comments?.length) return <Loading>Comments loading...</Loading>;
+
   return (
-      <section className='comment-section'>
-        <h3>Recent Comments</h3>
-        <ul>
-            {comments.map((comment) => (
-                <CommentItems key={comment?.id} comment={comment} />
-            ))}
-        </ul>
+    <section className="comment-section">
+      <h3>Recent Comments</h3>
+      <ul>
+        {comments.map((comment) => (
+          // const {postId, id, name, email, body } = comment
+          <CommentItems
+            key={comment?.id}
+            comment={comment.body}
+            email={comment.email}
+          />
+        ))}
+      </ul>
     </section>
-  )
+  );
 }
 
-export default CommentList
+export default CommentList;
