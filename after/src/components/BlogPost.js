b9d60e1f3c9ee5171a6728bd80c29f76e2d4bd87
Rakesh Kumar 10 <rakkumar0@WKMZTBDE5D14.global.publicisgroupe.net>
2024-05-30 10:28:04 +0530

    did apply pritter

--- after/src/components/BlogPost.js
+++ after/src/components/BlogPost.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import PostDetail from './PostDetail';
-import CommentList from './CommentList';
-import AddComment from './AddComment';
+import React from "react";
+import PostDetail from "./PostDetail";
+import CommentList from "./CommentList";
+import AddComment from "./AddComment";
 
 export default function BlogPost({ postId }) {
-
   return (
-    <div className='blog-post-wrapper'>
+    <div className="blog-post-wrapper">
       <PostDetail postId={postId} />
       <CommentList postId={postId} />
       <AddComment postId={postId} />
--- after/src/components/PostDetail.js
+++ after/src/components/PostDetail.js
@@ -1,28 +1,30 @@
-import React, {useState, useEffect} from 'react'
-import axios from 'axios';
-import Loading from './Loading';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Loading from "./Loading";
 
-function PostDetail({postId}) {
+function PostDetail({ postId }) {
   const [post, setPost] = useState(null);
 
   useEffect(() => {
-        async function fetchPost() {
-          const response = await axios.get(`https://jsonplaceholder.typicode.com/posts/${postId}`);
-          setPost(response.data);
-        }
-    
-        fetchPost();
-    }, [postId]);
+    async function fetchPost() {
+      const response = await axios.get(
+        `https://jsonplaceholder.typicode.com/posts/${postId}`
+      );
+      setPost(response.data);
+    }
+
+    fetchPost();
+  }, [postId]);
 
   if (!post) return <Loading>Post detail loading...</Loading>;
-      
+
   return (
-    <section className='post-detail'>
-        <h2>{post.title}</h2>
-        <p>{post.body}</p>
-        <a href='#'>Read More</a>
+    <section className="post-detail">
+      <h2>{post.title}</h2>
+      <p>{post.body}</p>
+      <a href="#">Read More</a>
     </section>
-  )
+  );
 }
 
-export default PostDetail
+export default PostDetail;
